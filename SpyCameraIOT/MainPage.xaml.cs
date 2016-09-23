e99eaf35441dab795b1f2b0a9ff32bbb66285d9f
Martone Rosario <rosario.martone@gmail.com>
2016-09-23 08:23:44 +0100

    Scaffolding v 1.0

--- SpyCameraIOT/MainPage.xaml.cs
+++ SpyCameraIOT/MainPage.xaml.cs
@@ -27,9 +27,94 @@ namespace SpyCameraIOT
             this.InitializeComponent();
         }
 
-        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        #region menu
+
+        private void OpenCloseMenu()
         {
             svMain.IsPaneOpen = !svMain.IsPaneOpen;
         }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenCloseMenu();
+        }
+
+        #endregion
+
+        #region menuSelection
+
+        private void OpenPage(string page)
+        {
+            switch(page)
+            {
+                case "Home":
+                    mainFrame.Navigate(typeof(Frames.fHome));
+
+                    break;
+                case "Account":
+                    mainFrame.Navigate(typeof(Frames.fAccount));
+
+                    break;
+                case "Live":
+                    mainFrame.Navigate(typeof(Frames.fLive));
+
+                    break;
+                case "Settings":
+                    mainFrame.Navigate(typeof(Frames.fSettings));
+
+                    break;
+                case "Info":
+                    mainFrame.Navigate(typeof(Frames.fInfo));
+
+                    break;
+                case "Feedback":
+                    mainFrame.Navigate(typeof(Frames.fFeedback));
+
+                    break;
+                case "Alert":
+                    mainFrame.Navigate(typeof(Frames.fAlert));
+
+                    break;
+            }
+
+            svMain.IsPaneOpen = false;
+        }
+
+        private void spHome_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            OpenPage("Home");
+        }
+
+        private void spAccount_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            OpenPage("Account");
+        }
+
+        private void spLive_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            OpenPage("Live");
+        }
+
+        private void spSettings_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            OpenPage("Settings");
+        }
+
+        private void spInfo_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            OpenPage("Info");
+        }
+
+        private void spFeedback_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            OpenPage("Feedback");
+        }
+
+        private void spAlert_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            OpenPage("Alert");
+        }
+
+        #endregion
     }
 }
