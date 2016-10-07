e05746ce4fd07ac972c8ad2721fb92da2a22f052
Martone Rosario <rosario.martone@gmail.com>
2016-10-07 11:10:14 +0100

    IOT devices setup

--- SpyCameraIOT/App.xaml.cs
+++ SpyCameraIOT/App.xaml.cs
@@ -1,9 +1,21 @@
 ﻿using SpyCameraIOT.Models;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.ApplicationInsights;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace SpyCameraIOT
@@ -19,8 +31,45 @@ namespace SpyCameraIOT
         /// </summary>
         public App()
         {
+            // Initialize AI telemetry in the app.
+            WindowsAppInitializer.InitializeAsync();
+
+            // Timer uploading hearbeat telemetry event
+            HeartbeatTimer = new DispatcherTimer();
+            HeartbeatTimer.Tick += HeartbeatTimer_Tick;
+            HeartbeatTimer.Interval = new TimeSpan(0, heartbeatInterval, 0);    // tick every heartbeatInterval
+            HeartbeatTimer.Start();
+
+            // Retrieve and set environment settings
+            OSVersion = EnvironmentSettings.GetOSVersion();
+            appVersion = EnvironmentSettings.GetAppVersion();
+            deviceName = EnvironmentSettings.GetDeviceName();
+            ipAddress = EnvironmentSettings.GetIPAddress();
+
             this.InitializeComponent();
             this.Suspending += OnSuspending;
+            this.Resuming += OnResuming;
+
+            Controller = new AppController();
+        }
+
+        /// <summary>
+        /// Invoked when the heartbeat timer ticks.
+        /// </summary>
+        void HeartbeatTimer_Tick(object sender, object e)
+        {
+            // Log telemetry event that the device is alive
+
+            TelemetryHelper.TrackMetric("DeviceHeartbeat", heartbeatInterval);
+        }
+
+        /// <summary>
+        /// Invoked when the application resumes from suspend.
+        /// </summary>
+        private async void OnResuming(Object sender, Object e)
+        {
+            GlobalStopwatch.Start();
+            await Controller.Initialize();
         }
 
         /// <summary>
@@ -28,14 +77,21 @@ namespace SpyCameraIOT
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected async override void OnLaunched(LaunchActivatedEventArgs e)
         {
+            GlobalStopwatch.Start();
+
+            await Controller.Initialize();
+
+            TelemetryHelper.TrackEvent("Application Launched");
+
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
+
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
@@ -56,18 +112,15 @@ namespace SpyCameraIOT
                 Window.Current.Content = rootFrame;
             }
 
-            if (e.PrelaunchActivated == false)
+            if (rootFrame.Content == null)
             {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
             }
+            // Ensure the current window is active
+            Window.Current.Activate();
         }
 
         /// <summary>
@@ -89,13 +142,30 @@ namespace SpyCameraIOT
         /// <param name="e">Details about the suspend request.</param>
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
+            // Track App suspension/exit:
+            GlobalStopwatch.Stop();
+            TelemetryHelper.TrackMetric("AppRuntime", GlobalStopwatch.Elapsed.TotalMilliseconds);
+
+            var metrics = new Dictionary<string, string> { { "appRuntime", GlobalStopwatch.Elapsed.TotalMilliseconds.ToString() } };
+            TelemetryHelper.TrackEvent("UserRuntime", metrics);
+
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
 
-#region personalizedMethods
+        #region personalizedMethods
 
+        private DispatcherTimer HeartbeatTimer;
+        public static AppController Controller;
+        public static Stopwatch GlobalStopwatch = Stopwatch.StartNew();
+        private int heartbeatInterval = 1;  // 1 min
+
+        // Environment settings
+        private string OSVersion = "";
+        private string appVersion = "";
+        private string deviceName = "";
+        private string ipAddress = "";
         public static Account account { get; set; }
 
         public static string IOTUrl = "SpyCameraIOT.azure-devices.net";
@@ -111,6 +181,12 @@ namespace SpyCameraIOT
         //IOT device
         //public static string deviceIDMobile = "Windows10IOT";
         //public static string deviceKeyMobile = "4N9pSNtuivosWRGRhaCO+y/cByMKbiK0qelniAO5PqU=";
+
+        public enum CameraType
+        {
+            Ip,
+            Usb
+        }
 
         public static string IsMobile
         {
